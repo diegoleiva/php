44ca3c6b5cacb05e7cf8046f6e5548a207bb7f1a
Diego <diegoleiva.1986@gmail.com>
2024-01-09 21:56:01 -0300

    Create sendMail.php
    v1.0

---
+++ commonFunctions/sendMail.php
@@ -0,0 +1,5 @@
+// Ejemplo de envío de correo electrónico
+$destinatario = "destinatario@example.com";
+$asunto = "Asunto del correo";
+$mensaje = "Contenido del correo";
+mail($destinatario, $asunto, $mensaje);
